49c562252e6cab845b304e511bbf8c954ec79a4d
Denis <lovipochtuarnold@gmail.com>
2015-09-28 14:48:44 +0300

    feat(app): 70%

--- app/scripts/calculatorCtrl.js
+++ app/scripts/calculatorCtrl.js
@@ -2,6 +2,8 @@ import $ from 'jquery';
 import {calcApp} from './app';
 
 $(function () {
+	'use strict';
+
 	calcApp.controller('calcCtrl',
 		function calcCtrl($scope) {
 
@@ -10,25 +12,51 @@ $(function () {
 			$scope.newNumber = true;
 			$scope.memory = '';
 
+			function refreshData(operate) {
+				$scope.newNumber = true;
+				$scope.memory = $scope.dispVal;
+				$scope.operate = operate;
+			}
+
 			$scope.updatepVal = function (num) {
 				if ($scope.dispVal === '0' || $scope.newNumber) {
 					$scope.dispVal = num;
 					$scope.newNumber = false;
 				}else {
-					$scope.dispVal += String(num);
+					$scope.dispVal = $scope.dispVal + String(num);
+				}
+				if (num === ' ' && $scope.dispVal !== '0') {
+					$scope.dispVal = $scope.dispVal.replace(num, '');
 				}
 			};
 
 			$scope.plus = function () {
-				$scope.newNumber = true;
-				$scope.memory = $scope.dispVal;
-				$scope.operate = '+';
+				refreshData('+');
 			};
 
 			$scope.minus = function () {
-				$scope.newNumber = true;
-				$scope.memory = $scope.dispVal;
-				$scope.operate = '-';
+				refreshData('-');
+			};
+
+			$scope.divide = function () {
+				refreshData('/');
+			};
+
+			$scope.multiply = function () {
+				refreshData('*');
+			};
+
+			$scope.percent = function () {
+				refreshData('%');
+			};
+
+			$scope.clearAll = function () {
+				$scope.dispVal = '0';
+				$scope.operate = '';
+			};
+
+			$scope.addComa = function () {
+				$scope.dispVal = $scope.dispVal + '.';
 			};
 
 			$scope.calculate = function () {
@@ -38,18 +66,22 @@ $(function () {
 				if ($scope.operate === '-') {
 					$scope.dispVal = parseFloat($scope.memory) - parseFloat($scope.dispVal);
 				}
+				if ($scope.operate === '/') {
+					$scope.dispVal = parseFloat($scope.memory) / parseFloat($scope.dispVal);
+				}
+				if ($scope.operate === '*') {
+					$scope.dispVal = parseFloat($scope.memory) * parseFloat($scope.dispVal);
+				}
+				if ($scope.operate === '%') {
+					$scope.dispVal = parseFloat($scope.memory) / parseFloat($scope.dispVal);
+					$scope.dispVal = $scope.dispVal * parseFloat('100%');
+					$scope.dispVal = Math.round($scope.dispVal);
+					$scope.dispVal = $scope.dispVal + '%';
+				}
 				$scope.operate = '';
 				$scope.newNumber = true;
 			};
 
-			$scope.clearAll = function () {
-				$scope.dispVal = '0';
-				$scope.operate = '';
-			};
-
-			$scope.addComa = function () {
-				$scope.dispVal += '.';
-			};
 		});
 });
 
